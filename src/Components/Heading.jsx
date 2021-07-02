442efbcbc32e4bcb9f89021105ac38ece378ed2b
Bilal Zak <bzak954@gmail.com>
2021-07-02 19:07:09 +0000

    add heading component

---
+++ src/Components/Heading.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+        <div>
+            <div className="head">
+                <h1>Codetrain Registration Form</h1>
+                <p style={{fontSize: '20px'}}>This form is the first step to register for Codetrain's
+                 program starting on 25th July 2020. Our admissions team will reach out to you after 
+                 successfully submitting your form.</p>
+                <h4>*Required</h4>
+	        </div>
+        </div>
+    );
+}
+
+export default Heading;
